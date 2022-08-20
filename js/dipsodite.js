c6b5f47006d18c79f918e72972c64d315557f26c
mdsifat <mdsifat7252@gmail.com>
2022-08-21 00:05:42 +0600

    added

--- js/dipsodite.js
+++ js/dipsodite.js
@@ -1,4 +1,8 @@
-document.getElementById('dbtn').addEventListener('click', function(){
+document.getElementById('wbtn').addEventListener('click', function(){
     const dvalue = document.getElementById('wfield').value;
     console.log(dvalue);
+    // document.getElementById('wfield').value=null;
+
+    const total_d =  document.getElementById('total-d').innerText;
+    document.getElementById('total-d').innerText = dvalue;
 });
